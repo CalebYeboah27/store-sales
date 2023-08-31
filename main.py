f1ebf4c0f3eaa07c0239282af233075dc3f74d43
Caleb Yeboah <caleb.yeboah@amalitech.org>
2023-08-31 17:55:53 +0000

    Add total price average

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
+products = ["Sankofa Foods", "Jamestown Coffee", "Bioko Chocolate", "Blue Skies Ice Cream",
+            "Fair Afric Chocolate", "Kawa Moka Coffee", "Aphro Spirit", "Mensado Bissap", "Voltic"]
+
+prices = [30, 25, 40, 20, 20, 35, 45, 50, 35]
+
+last_week = [2, 3, 5, 8, 4, 4, 6, 2, 9]
+
 # total price average for all products
+total_price = 0
+for price in prices:
+    total_price += price
+
+
+total_price_average = round(total_price / len(prices), 2)
+print(total_price, total_price_average)
+
+
 
